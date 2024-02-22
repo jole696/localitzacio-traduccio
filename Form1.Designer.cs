3e596379329a4f611fde83d911748fef3e296ae8
Jose Lendines <jole696@vidalibarraquer.net>
2024-02-22 03:25:57 -0800

    commit2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,102 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtAmount = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.SuspendLayout();
+            txtAmount = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button1 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            comboBox1 = new ComboBox();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
             // 
             // txtAmount
             // 
-            this.txtAmount.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtAmount.Location = new System.Drawing.Point(111, 37);
-            this.txtAmount.Name = "txtAmount";
-            this.txtAmount.Size = new System.Drawing.Size(121, 36);
-            this.txtAmount.TabIndex = 0;
+            txtAmount.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            txtAmount.Location = new Point(27, 60);
+            txtAmount.Margin = new Padding(4);
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(280, 36);
+            txtAmount.TabIndex = 0;
+            txtAmount.TextChanged += txtAmount_TextChanged;
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(99, 87);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(72, 56);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "2";
-            this.button2.UseVisualStyleBackColor = true;
+            button2.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(127, 296);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 78);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(177, 87);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(72, 56);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "3";
-            this.button3.UseVisualStyleBackColor = true;
+            button3.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(228, 296);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Size = new Size(93, 78);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(12, 87);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(72, 56);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(13, 296);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 78);
+            button1.TabIndex = 4;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(12, 165);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(72, 56);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.teclat_Click);
+            button4.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(13, 199);
+            button4.Margin = new Padding(4);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 78);
+            button4.TabIndex = 5;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += teclat_Click;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(289, 175);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            button5.Location = new Point(380, 204);
+            button5.Margin = new Padding(4);
+            button5.Name = "button5";
+            button5.Size = new Size(96, 32);
+            button5.TabIndex = 6;
+            button5.Text = "Convertir";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // comboBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(287, 27);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(80, 23);
-            this.comboBox1.TabIndex = 7;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(354, 167);
+            comboBox1.Margin = new Padding(4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(146, 29);
+            comboBox1.TabIndex = 7;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.TextChanged += comboBox1_TextChanged;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(127, 199);
+            button6.Margin = new Padding(4);
+            button6.Name = "button6";
+            button6.Size = new Size(93, 78);
+            button6.TabIndex = 8;
+            button6.Text = "5";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(228, 199);
+            button7.Margin = new Padding(4);
+            button7.Name = "button7";
+            button7.Size = new Size(93, 78);
+            button7.TabIndex = 9;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(13, 113);
+            button8.Margin = new Padding(4);
+            button8.Name = "button8";
+            button8.Size = new Size(93, 78);
+            button8.TabIndex = 10;
+            button8.Text = "7";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(127, 113);
+            button9.Margin = new Padding(4);
+            button9.Name = "button9";
+            button9.Size = new Size(93, 78);
+            button9.TabIndex = 11;
+            button9.Text = "8";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button10.Location = new Point(228, 113);
+            button10.Margin = new Padding(4);
+            button10.Name = "button10";
+            button10.Size = new Size(93, 78);
+            button10.TabIndex = 12;
+            button10.Text = "9";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button11.Location = new Point(13, 382);
+            button11.Margin = new Padding(4);
+            button11.Name = "button11";
+            button11.Size = new Size(207, 78);
+            button11.TabIndex = 13;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button12.Location = new Point(228, 382);
+            button12.Margin = new Padding(4);
+            button12.Name = "button12";
+            button12.Size = new Size(93, 78);
+            button12.TabIndex = 14;
+            button12.Text = "erease";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 21);
+            label1.TabIndex = 15;
+            label1.Text = "Conversor";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(354, 130);
+            comboBox2.Margin = new Padding(4);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(146, 29);
+            comboBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(355, 60);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(144, 36);
+            textBox1.TabIndex = 18;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 381);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.txtAmount);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 533);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(label1);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(comboBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(txtAmount);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -135,5 +271,15 @@
         private Button button4;
         private Button button5;
         private ComboBox comboBox1;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Label label1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,11 +12,29 @@ namespace demo_calculadora
             button1.Click += new EventHandler(teclat_Click);
             button2.Click += new EventHandler(teclat_Click);
             button3.Click += new EventHandler(teclat_Click);
+            button6.Click += new EventHandler(teclat_Click);
+            button7.Click += new EventHandler(teclat_Click);
+            button8.Click += new EventHandler(teclat_Click);
+            button9.Click += new EventHandler(teclat_Click);
+            button10.Click += new EventHandler(teclat_Click);
+            button11.Click += new EventHandler(teclat_Click);
+
+            
+            button12.Click += new EventHandler(erease);
+
+
+            button5.Click += new EventHandler(convertir);
+
+
 
             // carregar items al listbox
             comboBox1.Items.Add("Euro");
             comboBox1.Items.Add("YEN");
             comboBox1.Items.Add("$AUS");
+
+            comboBox2.Items.Add("Euro");
+            comboBox2.Items.Add("YEN");
+            comboBox2.Items.Add("$AUS");
 
             //
             //accedir a constants d'una altra classe
@@ -25,6 +43,79 @@ namespace demo_calculadora
 
             //gestió combobox
             comboBox1.Text = "Tria una opció";
+            comboBox2.Text = "Tria una opció";
+
+        }
+        private void erease(object sender, EventArgs e)
+        {
+            txtAmount.Text = "";
+
+        }
+        private void convertir(object sender, EventArgs e)
+        {
+            string text = txtAmount.Text;
+
+            if (comboBox1.SelectedIndex < 0 || comboBox2.SelectedIndex < 0 || string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Necesitas seleccionar las 2 unidades e introducir numeros");
+            } else
+            {
+                double numero = double.Parse(text);
+                string selectedItem1 = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                string selectedItem2 = comboBox2.Items[comboBox2.SelectedIndex].ToString();
+
+                //Euro a yen
+                if (selectedItem1 == "Euro" && selectedItem2 == "YEN")
+                {
+                    double tasaDeCambio = 163.04;
+                    numero = numero * tasaDeCambio;
+                }
+                //Euro a $AUS
+                if (selectedItem1 == "Euro" && selectedItem2 == "$AUS")
+                {
+                    double tasaDeCambio = 1.65;
+                    numero = numero * tasaDeCambio;
+                }
+                //YEN a Euro
+                if (selectedItem1 == "YEN" && selectedItem2 == "Euro")
+                {
+                    double tasaDeCambio = 0.0061;
+                    numero = numero * tasaDeCambio;
+
+                }
+                //YEN a $AUS
+                if (selectedItem1 == "YEN" && selectedItem2 == "$AUS")
+                {
+                    double tasaDeCambio = 0.010;
+                    numero = numero * tasaDeCambio;
+
+                }
+                //$AUS a Euro
+                if (selectedItem1 == "$AUS" && selectedItem2 == "Euro")
+                {
+                    double tasaDeCambio = 0.61;
+                    numero = numero * tasaDeCambio;
+                }
+                //$AUS a YEN
+                if (selectedItem1 == "$AUS" && selectedItem2 == "YEN")
+                {
+                    double tasaDeCambio = 98.89;
+                    numero = numero * tasaDeCambio;
+                }
+
+                if (selectedItem1 == selectedItem2)
+                {
+                    MessageBox.Show("No puedes tranformar el mismo tipo de moneda");
+                } else
+                {
+                    textBox1.Text = numero.ToString();
+                }
+
+
+            }
+
+
+
         }
 
         private void teclat_Click(object sender, EventArgs e)
@@ -34,6 +125,7 @@ namespace demo_calculadora
             txtAmount.Text += number;
 
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
 
@@ -44,8 +136,6 @@ namespace demo_calculadora
 
         private void button5_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(string.Format("Has triat índex {0} ", comboBox1.SelectedIndex.ToString));
-            //txtAmount.Text = array2D[0, 0];
 
         }
 
@@ -67,6 +157,36 @@ namespace demo_calculadora
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
         {
 
         }
